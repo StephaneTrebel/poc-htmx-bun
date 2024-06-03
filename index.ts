141296cc7d1d0f1868e2e80d4b4564a863186d85
Stéphane TRÉBEL (WeScale) <stephane.trebel@wescale.fr>
2024-06-03 10:54:52 +0200

    Add basic Fastify server

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import Fastify from 'fastify';
+
+const fastify = Fastify({ logger: true });
+
+fastify.get('/', async function handler(_request, _response) {
+  return { hello: 'world' };
+});
+
+try {
+  await fastify.listen({ port: 8080 });
+} catch (error) {
+  fastify.log.error(error);
+  process.exit(1);
+}
